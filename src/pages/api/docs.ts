e0638be64f8d03a672bf8585c85e147576e64a4a
Diego <diego.arroniz@accenture.com>
2025-04-04 10:35:46 -0600

    feat: added history post and get

--- src/pages/api/docs.ts
+++ src/pages/api/docs.ts
@@ -2,7 +2,7 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerOptions from "../../../lib/swaggerConfig.cjs";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const swaggerSpec = swaggerJSDoc(swaggerOptions);
   res.status(200).json(swaggerSpec);
 }
