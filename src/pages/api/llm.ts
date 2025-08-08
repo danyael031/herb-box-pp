292899a4c94e07bb39e5ec433b3ad060f8f3ffa7
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2025-08-08 09:49:27 -0700

    Adding support for user message

--- src/pages/api/llm.ts
+++ src/pages/api/llm.ts
@@ -1,14 +1,17 @@
 import { agentApp } from "@/agentApp";
+import { LLMReqBody } from "@/types/llm";
 import { HumanMessage } from "@langchain/core/messages";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if(req.method === 'POST'){
+    let userRequest = req.body as LLMReqBody;
+    const agentFinalState = await agentApp.invoke(
+      { messages: [new HumanMessage(userRequest.userMessage)] },
+      { configurable: { thread_id: "default"} },
+    );
 
-  const agentFinalState = await agentApp.invoke(
-    { messages: [new HumanMessage("what is the current weather in sf")] },
-    { configurable: { thread_id: 42 } },
-  );
-
-  res.status(200).json({response: agentFinalState.messages[agentFinalState.messages.length - 1].content})
+    res.status(200).json({response: agentFinalState.messages[agentFinalState.messages.length - 1].content})
+  }
 
 }
