a3194a747a92e8a1b5f3d1d14f02c84d85a86dc4
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2025-08-08 10:32:08 -0700

    Adding plants prompt

--- src/pages/api/llm.ts
+++ src/pages/api/llm.ts
@@ -1,13 +1,28 @@
 import { agentApp } from "@/agentApp";
 import { LLMReqBody } from "@/types/llm";
-import { HumanMessage } from "@langchain/core/messages";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if(req.method === 'POST'){
     let userRequest = req.body as LLMReqBody;
+
+    const sensorsValues = {
+      temperature: "45",
+      ambient_humidity: "10%",
+      soil_humidity: "10%"
+    }
+
+    const systemInputSensors = `
+Entrada:
+
+Temperatura: ${sensorsValues.temperature}
+Humedad ambiental: ${sensorsValues.ambient_humidity}
+Humedad de tierra: ${sensorsValues.soil_humidity}
+    `
+
     const agentFinalState = await agentApp.invoke(
-      { messages: [new HumanMessage(userRequest.userMessage)] },
+      { messages: [new SystemMessage(systemInputSensors) ,new HumanMessage(userRequest.userMessage)] },
       { configurable: { thread_id: "default"} },
     );
 
