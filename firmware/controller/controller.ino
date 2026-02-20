8930931fe7c10099bc87219909b4e69202f5454f
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2026-02-20 10:19:05 -0700

    Send random sensor values from Arduino

--- firmware/controller/controller.ino
+++ firmware/controller/controller.ino
@@ -1,15 +1,47 @@
-
-
+const String SOLICITUD_SENSORES = "S";
 
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-
+  // Espera opcional a que el puerto serial esté listo (útil en algunos entornos)
+  while (!Serial) {
+    ; 
+  }
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
+void randomSensorValues(String out[3]) {
+  for (byte i = 0; i < 3; i++) {
+    out[i] = String(random(20, 31)); // [20, 30]
+  }
+}
 
-  Serial.println("\{\"plantId\": 1,\"airHumidity\": 50,\"groundHumidity\": 50,\"temperature\": 20\}");
+void loop() {
+  String sensorValues[3];
+
   delay(5*1000);  // delay in between reads for stability
+
+  randomSensorValues(sensorValues);
+  Serial.print("\{\"plantId\": 1,\"airHumidity\": ");
+  Serial.print(sensorValues[0]);
+  Serial.print(",\"groundHumidity\": ");
+  Serial.print(sensorValues[1]);
+  Serial.print(",\"temperature\": ");
+  Serial.print(sensorValues[2]);
+  Serial.println("\}");
+  //if (Serial.available() > 0) {
+  //  // Leer el mensaje completo hasta salto de línea
+  //  String mensaje = Serial.readStringUntil('\n');
+  //  mensaje.trim(); // Elimina espacios y saltos de línea
+
+  //  if (mensaje == SOLICITUD_SENSORES) {
+  //    randomSensorValues(sensorValues);
+  //    Serial.print("\{\"plantId\": 1,\"airHumidity\": ");
+  //    Serial.print(sensorValues[0]);
+  //    Serial.print(",\"groundHumidity\": ");
+  //    Serial.print(sensorValues[1]);
+  //    Serial.print(",\"temperature\": ");
+  //    Serial.print(sensorValues[2]);
+  //    Serial.println("\}");
+
+  //  }
+  //}
 }
